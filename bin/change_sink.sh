39ea67c183f36f2cf99cd025c8e7e49b583c7359
Talha <talha.kerpicci@gmail.com>
2021-10-10 15:50:09 +0300

    Update

--- bin/change_sink.sh
+++ bin/change_sink.sh
@@ -1,9 +1,9 @@
-current_sink="$(cat .audio_sink)"
+current_sink="$(cat ~/.audio_sink)"
 
 if [ "$current_sink" = "alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1" ]; then
 	pacmd set-default-sink alsa_output.pci-0000_00_1b.0.analog-stereo
-	echo "alsa_output.pci-0000_00_1b.0.analog-stereo" > .audio_sink
+	echo "alsa_output.pci-0000_00_1b.0.analog-stereo" > ~/.audio_sink
 else
 	pacmd set-default-sink alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1
-	echo "alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1" > .audio_sink
+	echo "alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1" > ~/.audio_sink
 fi
